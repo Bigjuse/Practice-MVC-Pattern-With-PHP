ec8675e50cdaecccef118e2394661ac7e9a78613
Rakib <aliazgarrakib@gmail.com>
2023-05-21 17:58:20 +0600

    3

--- src/app/Views/register.php
+++ src/app/Views/register.php
@@ -1,14 +1,16 @@
 <h1>Create an Account</h1>
 <form action="/register" method="post">
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Name</label>
-        <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label class="form-label">Name</label>
+        <input type="text" name="name" value="<?= $model->name ?? '' ?>" class="form-control <?= $model->hasError() ?>">
     </div>
+
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" name="email" class="form-control">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
+
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
         <input type="password" name="password" class="form-control" id="exampleInputPassword1">
